6e09fcd30036c5043bcab02ca92c91390d152b88
suihiso <valery.g0294@gmail.com>
2024-04-12 13:27:24 +0300

    first commit

---
+++ question_5.js
@@ -0,0 +1,19 @@
+const inputAnswer = document.querySelector(".inputAnswer");
+const sendButton = document.querySelector(".sendButton");
+const hintButton = document.querySelector(".hintButton");
+const errorMessage = document.getElementById("error");
+
+sendButton.addEventListener("click", function() {
+    if (inputAnswer.value != "" & inputAnswer.value == "да")
+        window.location.href = "quest_completed.html";
+    else {
+        errorMessage.style.display = "block";
+        setTimeout(function() {
+            errorMessage.style.display = "none";
+        }, 3000);
+    }
+});
+
+hintButton.addEventListener("click", function() {
+    window.alert("Текст подсказки");
+});
